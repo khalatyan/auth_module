d02bf8acca9747274e7d161c3f1e6a94a34dee74
khalatyan <khalatyaaan@gmail.com>
2021-05-12 00:28:18 +0800

    new module

--- auth_module/settings.py
+++ auth_module/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -72,13 +74,13 @@ WSGI_APPLICATION = 'auth_module.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -119,7 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-try:
-    from auth_module.local_settings import *
-except ImportError:
-    pass
+from auth_module.local_settings import *
